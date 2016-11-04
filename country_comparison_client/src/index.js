e0af923dec0da831c746ae7606b23be320f10291
Mark Robertson <markrivasrobertson@yahoo.com>
2016-11-04 15:01:11 -0400

    test initial hookup to api via jquery ajax calls

--- country_comparison_client/src/index.js
+++ country_comparison_client/src/index.js
@@ -1,1 +1,19 @@
 console.log('hooked up');
+
+function checkIt(num1, num2) {
+  $.ajax(`http://localhost:3000/api/v1/countries/${num1}`)
+    .then((responseOne) => {
+      console.log(responseOne);
+      console.log('response 1');
+    })
+    .then($.ajax(`http://localhost:3000/api/v1/countries/${num2}`)
+    .then((responseTwo) => {
+      console.log(responseTwo);
+      console.log('response 2');
+    }));
+}
+
+const button = document.getElementById('testButton');
+button.addEventListener('click', () => {
+  checkIt(67, 39);
+});
