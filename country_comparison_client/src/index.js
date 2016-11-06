e28181cbaa4070294857f44dc2eecefe125fe45b
Mark Robertson <markrivasrobertson@yahoo.com>
2016-11-06 11:32:27 -0500

    alter original jQuery ajax requests to replace them with superagent.  Remove jQuery CDN script tag from index.html

--- country_comparison_client/src/index.js
+++ country_comparison_client/src/index.js
@@ -1,16 +1,34 @@
-console.log('hooked up');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import request from 'superagent';
 
 function checkIt(num1, num2) {
-  $.ajax(`http://localhost:3000/api/v1/countries/${num1}`)
+  let value = 0;
+  let values = []
+  request.get(`http://localhost:3000/api/v1/countries/${num1}`)
     .then((responseOne) => {
-      console.log(responseOne);
-      console.log('response 1');
+      const parsedResponse = JSON.parse(responseOne.text);
+      value = parsedResponse.name;
+      values.push(value);
+      console.log(values);
+      console.log('array');
     })
-    .then($.ajax(`http://localhost:3000/api/v1/countries/${num2}`)
+    .then(request.get(`http://localhost:3000/api/v1/countries/${num2}`)
     .then((responseTwo) => {
-      console.log(responseTwo);
-      console.log('response 2');
-    }));
+      const parsedResponse = JSON.parse(responseTwo.text);
+      value = parsedResponse.name;
+      values.push(value);
+      console.log(values);
+      console.log('array');
+      d3.select('.root')
+        .selectAll('div')
+          .data(values)
+        .enter().append('div')
+          .style('height', '20px')
+          .style("width", function(d) { return (d.length*10)+"px"; })
+          .style('background-color', '#ff0000')
+          .text(function(d) { return d; })
+    }))
 }
 
 const button = document.getElementById('testButton');
